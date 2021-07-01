c60923f2119698860b463f71e556b8777e95e376
themancodes <menocorde04@gmail.com>
2021-07-01 18:41:49 -0500

    Feat: Rebuilding Basic Room Layouts

---
+++ front-end/src/Components/Home/Reservations.jsx
@@ -0,0 +1,17 @@
+export default function Reservations( ) {
+
+  return(
+    <div>
+      <p>Martes 8:00 am a 12pm</p>
+      <p>Miercoles 8:00 am a 12pm</p>
+      <p>Jueves 8:00 am a 12pm</p>
+      <input type="calendar"/>
+      <button> + </button>
+    </div>
+  )
+// TODO:
+// fetch reservations
+// updatestate from inputs with use effect 
+// POST from inputs
+// toggle: on click + ( ) => display calendar input 
+}
